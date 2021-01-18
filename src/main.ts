ce5b73c294afe6bd4a6869ffb834e4134d5d60af
canvascat <canvascat@qq.com>
2021-01-18 15:01:34 +0800

    + router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
+import router from './router'
+import 'element-plus/lib/theme-chalk/index.css';
+import './assets/common.styl'
 
-createApp(App).mount('#app')
+const app = createApp(App).use(router).use(ElementPlus)
+app.mount('#app')
---
+++ src/router.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/CanvasMosaic'
+  },
+  {
+    path: '/HelloWorld',
+    name: 'HelloWorld',
+    component: () => import('./components/HelloWorld.vue')
+  },
+  {
+    path: '/CanvasMosaic',
+    name: 'CanvasMosaic',
+    component: () => import('./components/CanvasMosaic.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes
+})
+
+export default router
