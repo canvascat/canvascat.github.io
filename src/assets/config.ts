5318c335663429150037fad5a84147865f04601d
canvascat <canvascat@qq.com>
2021-02-05 11:45:46 +0800

    + style

---
+++ src/assets/config.ts
@@ -0,0 +1,8 @@
+export const menus = [
+  {title: '首页', url: '/'},
+  {title: '搜索', url: '/CanvasMosaic'},
+  {title: '归类', url: '/ImageEditor'},
+  {title: '友链', url: '/pages/links'},
+  {title: '留言', url: '/pages/chat'},
+  {title: '关于', url: '/pages/about'}
+]
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import DemoBlock from '@/components/demo-block.vue'
 import 'element-plus/lib/theme-chalk/index.css'
-import './assets/common.scss'
+import '@/assets/styles/common.scss'
+import '@/assets/styles/page.scss'
+import '@/assets/styles/post.scss'
+import '@/assets/styles/theme-dark.scss'
 
-const app = createApp(App).use(router).use(ElementPlus)
+const app = createApp(App)
+app.component(DemoBlock.name, DemoBlock)
+app.use(router);
+app.use(ElementPlus)
 app.mount('#app')
