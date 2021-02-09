2638ce1bb403285bcd4805b258f5b98eb8cdc789
canvascat <canvascat@qq.com>
2021-02-09 18:57:16 +0800

    + rafThrottle

--- src/util/util.ts
+++ src/util/util.ts
@@ -11,3 +11,17 @@ if (window.matchMedia) {
 }
 
 export const sleep = (t = 0) => new Promise(resolve => setTimeout(resolve, t))
+
+
+export type AnyFunction<T> = (...args: any[]) => T
+export function rafThrottle<T extends AnyFunction<any>>(fn: T): AnyFunction<void> {
+  let locked = false
+  return function(...args: any[]) {
+    if (locked) return
+    locked = true
+    window.requestAnimationFrame(() => {
+      fn.apply(this, args)
+      locked = false
+    })
+  }
+}
