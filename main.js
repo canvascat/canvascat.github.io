3cf65e9b3bef113ce323df0974f94cf150f9c183
yanzhuang <canvascat@qq.com>
2024-10-30 13:43:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-
-const module = await import('https://unpkg.com/svgo@3.3.2/dist/svgo.browser.js')
+const module = await import(
+  "https://unpkg.com/svgo@3.3.2/dist/svgo.browser.js"
+);
 /** @type {import('svgo').optimize} */
-const optimize = module.optimize
-await import('https://unpkg.com/xss@1.0.15/dist/xss.js')
+const optimize = module.optimize;
+await import("https://unpkg.com/xss@1.0.15/dist/xss.js");
 
-const input = document.querySelector('input')
+const input = document.querySelector("input");
 
-const current = Object.create(null)
+const current = Object.create(null);
 
-input.addEventListener('change', async event => {
-  const file = input.files?.item(0)
+input.addEventListener("change", async (event) => {
+  const file = input.files?.item(0);
   if (file === current.file) return;
   current.file = file;
   if (!file) return;
@@ -17,36 +18,47 @@ input.addEventListener('change', async event => {
   const output = optimize(content, {
     js2svg: {
       pretty: true,
-      indent: 4
+      indent: 4,
     },
 
     plugins: [
       {
-        name: 'preset-default',
+        name: "preset-default",
         params: {
           overrides: {
             removeUselessStrokeAndFill: {
               stroke: true,
               fill: true,
-              removeNone: true
-            }
+              removeNone: true,
+            },
           },
         },
       },
       {
         name: "removeAttrs",
         params: {
-          attrs: 'fill',
-        }
-      }
-    ]
-  }).data
+          attrs: "fill",
+        },
+      },
+    ],
+  }).data;
 
-  const markupElements = [...document.querySelectorAll('.markup')]
-  markupElements[0].innerHTML = `<pre>${filterXSS(content)}</pre>`
-  markupElements[1].innerHTML = `<pre>${filterXSS(output)}</pre>`
+  const markupElements = [...document.querySelectorAll(".markup")];
+  markupElements[0].innerHTML = `<pre>${filterXSS(content)}</pre>`;
+  markupElements[1].innerHTML = `<pre>${filterXSS(output)}</pre>`;
 
-  const renderElements = [...document.querySelectorAll('.render')]
-  renderElements[0].innerHTML = content
-  renderElements[1].innerHTML = output
-})
+  const renderElements = [...document.querySelectorAll(".render")];
+  renderElements[0].innerHTML = content;
+  renderElements[1].innerHTML = output;
+});
+
+(function remTestSetup() {
+  const wrap = document.querySelector("#rem-test");
+  if (!wrap) return;
+  wrap
+    .querySelector("button")
+    .addEventListener("click", () => [
+      (document.documentElement.style.fontSize =
+        wrap.querySelector("input").value),
+    ]);
+})();
